6e6a6364c38d45022d2cb2f972a4235dc623d978
Stephen Cotton <scotton@emersonecologics.com>
2021-02-16 10:46:42 -0500

    Adds properties array

--- src/Entities/Context.php
+++ src/Entities/Context.php
@@ -24,6 +24,9 @@ class Context
     /** @var string */
     protected $hostName;
 
+    /** @var array */
+    protected $properties;
+
     /**
      * Gets the context user ID
      * @return string|null
@@ -122,5 +125,47 @@ class Context
     {
         $this->hostName = $hostName;
         return $this;
+    }
+
+    /**
+     * Gets all properties
+     * @return array
+     */
+    public function getProperties(): ?array
+    {
+        return $this->properties;
+    }
+
+    /**
+     * Set a single property
+     * @param string $key
+     * @param string $value
+     * @return Context
+     */
+    public function setProperty(string $key, string $value): Context
+    {
+        $this->properties[$key] = $value;
+        return $this;
+    }
+
+    /**
+     * Remove a single property
+     * @param string $key
+     * @return Context
+     */
+    public function removeProperty(string $key): Context
+    {
+        unset($this->properties[$key]);
+        return $this;
+    }
+
+    /**
+     * Get a single property
+     * @param string $key
+     * @return string
+     */
+    public function getProperty(string $key): ?string
+    {
+        return $this->properties[$key];
     }
 }
